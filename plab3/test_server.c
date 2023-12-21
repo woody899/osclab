8a1aa5170b4e6e23653f5e1402fa392c700d16d7
Adham <adham.almeadawy@student.kuleuven.be>
2023-12-21 12:11:50 +0100

    plab 3 completed

--- plab3/test_server.c
+++ plab3/test_server.c
@@ -1,5 +1,5 @@
 /**
- * \author {AUTHOR}
+ * \author {Woody}
  */
 
 #include <stdio.h>
@@ -7,31 +7,15 @@
 #include <inttypes.h>
 #include "config.h"
 #include "lib/tcpsock.h"
+#include <pthread.h>
 
-/**
- * Implements a sequential test server (only one connection at the same time)
- */
 
-int main(int argc, char *argv[]) {
-    tcpsock_t *server, *client;
+
+void* handle_client(void* arg){
+    tcpsock_t *client = (tcpsock_t *)arg;
     sensor_data_t data;
     int bytes, result;
-    int conn_counter = 0;
-    
-    if(argc < 3) {
-    	printf("Please provide the right arguments: first the port, then the max nb of clients");
-    	return -1;
-    }
-    
-    int MAX_CONN = atoi(argv[2]);
-    int PORT = atoi(argv[1]);
 
-    printf("Test server is started\n");
-    if (tcp_passive_open(&server, PORT) != TCP_NO_ERROR) exit(EXIT_FAILURE);
-    do {
-        if (tcp_wait_for_connection(server, &client) != TCP_NO_ERROR) exit(EXIT_FAILURE);
-        printf("Incoming client connection\n");
-        conn_counter++;
         do {
             // read sensor ID
             bytes = sizeof(data.id);
@@ -47,17 +31,59 @@ int main(int argc, char *argv[]) {
                        (long int) data.ts);
             }
         } while (result == TCP_NO_ERROR);
-        if (result == TCP_CONNECTION_CLOSED)
+        if (result == TCP_CONNECTION_CLOSED) {
             printf("Peer has closed connection\n");
-        else
-            printf("Error occured on connection to peer\n");
+
+        }
+        else {
+            printf("Error occurred on connection to peer\n");
+        }
         tcp_close(&client);
-    } while (conn_counter < MAX_CONN);
+
+    pthread_exit(EXIT_SUCCESS);
+}
+
+
+int main(int argc, char *argv[]) {
+    tcpsock_t *server, *client;
+    int MAX_CONN = atoi(argv[2]);
+    int PORT = atoi(argv[1]);
+    int conn_counter = 0;
+    pthread_t thread[MAX_CONN];
+
+    if(argc < 3) {
+    	printf("Please provide the right arguments: first the port, then the max nb of clients");
+    	return -1;
+    }
+
+
+    if (tcp_passive_open(&server, PORT) != TCP_NO_ERROR) exit(EXIT_FAILURE);
+    printf("Test server is started\n");
+
+    while (1) {
+        if(conn_counter >= MAX_CONN){
+            break;
+        }
+
+        if (tcp_wait_for_connection(server, &client) != TCP_NO_ERROR) break;
+        printf("Incoming client connection\n");
+
+        if (pthread_create(&thread[conn_counter], NULL, handle_client, client) != 0) {
+            printf("Failed to create thread\n");
+            break;
+        }
+
+        conn_counter++;
+    }
+
+    for (int i = 0; i < conn_counter; ++i) {
+        if (pthread_join(thread[i], NULL) != 0) {
+            fprintf(stderr, "Error joining thread %d\n", i);
+        }
+    }
+
+
     if (tcp_close(&server) != TCP_NO_ERROR) exit(EXIT_FAILURE);
     printf("Test server is shutting down\n");
     return 0;
 }
-
-
-
-
